20a3d9abb80dc8b3cd3effa55c6705b3923fda55
zik <zik-saw@yandex.ru>
2018-01-09 14:41:31 +0500

    add fancybox galler 14:41

--- js/information.js
+++ js/information.js
@@ -2,13 +2,16 @@
 
     requestCheckAuth("information");
     var html = "";
+    var pictures = [];
 
     $('.information-left').children().each( function(){
         html += "<div class='slide'>"+$(this).prop('outerHTML')+"</div>";
+        pictures.push({ src : $(this).find('img').attr('src') });
     });
 
     $('.information-right').children().each( function(){
         html += "<div class='slide'>"+$(this).prop('outerHTML')+"</div>";
+        pictures.push({ src : $(this).find('img').attr('src') });
     });
     $('.information-slider').html(html);
 
@@ -20,15 +23,25 @@
 
     });
 
-    $("a#example1").fancybox();
 
     $('button[data-action]').click( function(){
         if($(this).attr('data-action') == "next" )
             $('.information-slider').slick('slickNext');
         else
             $('.information-slider').slick('slickPrev');
-
-
     });
+
+
+    $('section.information').on('click' , 'a[id-picture]' , function(){
+
+        $.fancybox.open( pictures , {
+            loop : true,
+            index: $(this).attr('id-picture'),
+        });
+    });
+
+
+
+
 
 })(jQuery);
--- js/production.js
+++ js/production.js
@@ -1,35 +1,47 @@
 ( function($){
 
-		requestCheckAuth("production");
-	
-		var html = "";
+	requestCheckAuth("production");
 
-		$('.production-left').children().each( function(){
-			html += "<div class='slide'>"+$(this).prop('outerHTML')+"</div>";
+	var html = "";
+	var pictures = [];
+
+	$('.production-left').children().each( function(){
+		html += "<div class='slide'>"+$(this).prop('outerHTML')+"</div>";
+		pictures.push({ src : $(this).find('img').attr('src') });
+	});
+
+	$('.production-right').children().each( function(){
+		html += "<div class='slide'>"+$(this).prop('outerHTML')+"</div>";
+		pictures.push({ src : $(this).find('img').attr('src') });
+	});
+	$('.production-slider').html(html);
+
+	$('.production-slider').slick({
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows:false,
+
+	});
+
+
+
+	$('button[data-action]').click( function(){
+		if($(this).attr('data-action') == "next" )
+			$('.production-slider').slick('slickNext');
+		else
+			$('.production-slider').slick('slickPrev');
+
+
+	});
+
+	$('section.production').on('click' , 'a[id-picture]' , function(){
+
+
+		$.fancybox.open( pictures , {
+			loop : true,
+			index: $(this).attr('id-picture'),
 		});
-	
-		$('.production-right').children().each( function(){
-			html += "<div class='slide'>"+$(this).prop('outerHTML')+"</div>";
-		});
-    	 $('.production-slider').html(html);
+	});
 
-		$('.production-slider').slick({
-			lazyLoad: 'ondemand',
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows:false,
-			
-		});
-
-		$("a#example1").fancybox();
-
-		$('button[data-action]').click( function(){
-				if($(this).attr('data-action') == "next" )
-					$('.production-slider').slick('slickNext');
-				else
-					$('.production-slider').slick('slickPrev');
-
-
-		});
-	
 })(jQuery);
