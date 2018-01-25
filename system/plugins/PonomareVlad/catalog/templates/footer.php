623d084965af56e776923ad8dd8baecf1db9006c
Владислав Пономарев <Public@PonomareVlad.ru>
2018-01-25 18:55:26 +0500

    Catalog templates

---
+++ system/plugins/PonomareVlad/catalog/templates/footer.php
@@ -0,0 +1,41 @@
+<!-- footer start -->
+<div class="footer">
+    <div class="footer-top">
+        <div class="footer-logo">
+            <a href="/"><img src="images/logo.png"></a>
+        </div>
+
+        <div class="footer-text">
+            <p>Производство детской одежды</p>
+            <p>Оптовая продажа</p>
+        </div>
+
+        <div class="footer-menu">
+            <ul>
+                <li><a href="/">Главная</a></li>
+                <li><a href="/registration.html">Регистрация</a></li>
+                <li><a href="/catalog/">Каталог</a></li>
+                <li><a href="collections.html">Коллекции</a></li>
+                <li><a href="information.html">О нас</a></li>
+                <li><a href="cooperation.html">Сотрудничество</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-contacts">
+            <div class="footer-phone">
+                <p><b>+7 (495)</b> 555 44 22</p>
+            </div>
+
+            <div class="footer-top-networks">
+                <button><img src="images/icons/insta.png"></button>
+                <button><img src="images/icons/fb.png"></button>
+                <button><img src="images/icons/vk.png"></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bot">
+        <p>© 2017 Cotton Baby</p>
+    </div>
+</div>
+<!-- footer end -->
