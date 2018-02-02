901c649426907a5d8a187e7ddee66fcddca1a76c
zik <zik-saw@yandex.ru>
2018-02-02 18:12:14 +0500

    Правки в contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -40,7 +40,7 @@ try{
 			}
 			catch (error)
 			{
-
+				requestSendBugs(error);
 			}
 
 
@@ -98,5 +98,30 @@ try{
 }
 catch (error)
 {
-	
+	requestSendBugs(error);
+}
+
+function isError(e){
+	return e && e.stack && e.message;
+}
+
+function requestSendBugs(error) {
+
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+
+	xhr.addEventListener("readystatechange", function () {
+		if (this.readyState === 4) {
+			console.log(this.responseText);
+		}
+	});
+
+	xhr.open("POST", window.location.protocol+"//"+"akula.cottonbaby.ru/system/extensions/errorCatcher/");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.setRequestHeader("Cache-Control", "no-cache");
+	xhr.setRequestHeader("Postman-Token", "6fc1aee4-6350-7914-1727-bb9cb2ab9235");
+	if(isError(error))
+		xhr.send(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+	else
+		xhr.send(JSON.stringify(error));
 }
